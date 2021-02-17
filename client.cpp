b0ea4b62a5bacf1e7b4fae91026ab7adf8bfdfc8
Conor Begley <conorbegley@outlook.com>
2021-02-17 13:03:34 +0000

    Example working with icon

--- client.cpp
+++ client.cpp
@@ -1,32 +1,47 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <arpa/inet.h> 
 #include <string>
 #include <iostream>
 #include <sys/socket.h>
 #include <unistd.h> 
 #include <netinet/in.h> 
+#include <arpa/inet.h> 
+#include <errno.h>
 
 using namespace std;
+
+int err_msg (int num, string msg){
+    if (num < 0 ){ 
+        cerr << "Error with "<< msg << "\n" << "Error Code:"<<errno << "\n"; 
+        exit(1);
+        }
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
     cout << "Starting Pi Client.";
-    int port = 51255;
+    string msg;
+    getline (cin, msg);
+    cout << msg ;
+    int port = 8080;
     int client_fd = socket(AF_INET,SOCK_STREAM, 0);
     struct sockaddr_in address;
     address.sin_family = AF_INET;
     address.sin_port = htons(port);
     int addrlen = sizeof(address);
+    
     int server_connect = inet_pton(AF_INET, "127.0.0.1", &address.sin_addr);
+    err_msg(server_connect, "server_address");
+    
     int connection = connect(client_fd, (struct sockaddr *)&address, sizeof(address));
-    if (connection != 0 ){ cerr << "Error connecting, Err Code : " << connection;}
-    send(client_fd , "Hello" , sizeof("Hello") , 0 ); 
-    char buffer[1024] ;
-    int valread = read(client_fd , buffer, 1024); 
-    cout << buffer;
-    shutdown(client_fd, SHUT_RDWR);
-    close(client_fd);
+    err_msg(connection, "server_connect");
+    
+    send(client_fd , msg.c_str(), sizeof(msg.c_str()) , 0 ); 
+    int shut = shutdown(client_fd, SHUT_RDWR);
+    err_msg(shut, "socket_shutdown");
+    int cls = close(client_fd);
+    err_msg(cls, "socket_close");
     return 0;
      
 }
